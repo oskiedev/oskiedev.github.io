9262ae2017ebc4693c0710fac59c7e7261966458
Oskie King <oskie.villarin@gmail.com>
2017-07-13 16:32:28 +0800

    updated

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+
+    // ##sliders
+    $('.hero-slider').bxSlider({
+        mode: 'fade',
+        speed: 1500,
+        pager: false,
+        controls: false,
+        auto: true,
+        pause: 6000
+    });
+
+});
